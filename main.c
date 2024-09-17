03f69da8e218ddda9359dc9d4d0318c2a48b3424
NesmaRamadan <nesmaramadan673@gmail.com>
2024-09-17 16:00:30 +0300

    V1.0.0

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define Hamada 10
+
+int main()
+{
+ int y=45;
+ int x=Hamada;
+ printf("x value=%d\n",x);
+ printf("y value=%d\n",y);
+ return 0;
+}
---
+++ main.s
@@ -0,0 +1,38 @@
+	.file	"main.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "x value=%d\12\0"
+.LC1:
+	.ascii "y value=%d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$45, -4(%rbp)
+	movl	$10, -8(%rbp)
+	movl	-8(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC1(%rip), %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
